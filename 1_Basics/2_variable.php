120a537484a8a1509556bfd0c3c75f5df1c1c327
nachatra-sharma <nakshusharm0@gmail.com>
2024-01-26 12:32:13 +0530

    1. Basics

--- 1_Basics/2_variable.php
+++ 1_Basics/2_variable.php
@@ -33,6 +33,7 @@ echo '<br/>';
 $foo = 'bar';
 $$foo = 'baz';
 echo $bar . ' ' . $$foo;
+echo '<br/>';
 
 // data types
 /**
@@ -46,4 +47,61 @@ echo $bar . ' ' . $$foo;
  * 7. Array
  * 8. Object
  */
+
+// 1. string
+$myName = "Nachatra Sharma";
+echo $myName . " this is a string";
+echo '<br/>';
+
+// 2. Integer
+$age = 21;
+echo $age . ' this is a int';
+echo '<br/>';
+
+// 3. Float
+$weight = 52.1;
+echo $weight . ' this is a float'; 
+echo '<br/>';
+
+// 4. Boolean
+$isReal = true;
+echo $isReal . ' this is a boolean';
+echo '<br/>';
+
+// 5. Null 
+$isNull = Null;
+echo $isNull;
+echo '<br/>';
+
+// 6. Array
+$arr = [1,2,3,4,5,6,7,'hello','naksh',true];
+echo print_r($arr);
+echo '<br/>';
+
+// checking type
+$fullName = 'Nachatra Sharma';
+echo gettype($fullName); 
+echo '<br/>';
+echo var_dump($fullName);
+echo '<br/>';
+
+// type casting
+function sum(int $first, $second) {
+    return $first + $second;
+}
+echo sum(3.5, 3);
+echo '<br/>';
+
+// strict mode in php
+
+// declare(strict_types = 1);
+function add(int $x, int $y)
+{
+    return $x + $y;
+}
+
+echo add(1, 2);
+echo '<br/>';
+
+
 ?>
